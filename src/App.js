12b0147a9454d479069a8f499ceaecc0cc8a9737
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-10-26 23:56:02 +0600

    practice done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+import {
+  Card,
+  Container,
+  H1,
+  P,
+  Tag,
+  ButtonContainer,
+  Image,
+  Content,
+} from "./Container.styled";
+import imgSrc from "./img/logo512.png";
+
 function App() {
   return (
-    <div className="container">hello</div>
+    <Container>
+      <Card>
+        <Content>
+          <Tag color="#4361ee">EXCLUSIVE</Tag>
+          <H1>React Styled Components</H1>
+          <P>
+            Exclusive React JS with styled components where you will learn why
+            we need this & how to use it
+          </P>
+          <ButtonContainer>
+            <a href="#">Watch now</a>
+            <a href="#">Github Repo</a>
+          </ButtonContainer>
+        </Content>
+        <Image src={imgSrc} alt="img" width="250px" />
+      </Card>
+    </Container>
   );
 }
 
