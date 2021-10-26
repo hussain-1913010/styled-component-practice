6ea957022eea867f9a65de4851f5c3c1839201da
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2021-10-27 00:25:44 +0600

    global component updated

--- src/App.js
+++ src/App.js
@@ -9,26 +9,30 @@ import {
   Content,
 } from "./Container.styled";
 import imgSrc from "./img/logo512.png";
+import GlobalStyle from "./Global.style";
 
 function App() {
   return (
-    <Container>
-      <Card>
-        <Content>
-          <Tag color="#4361ee">EXCLUSIVE</Tag>
-          <H1>React Styled Components</H1>
-          <P>
-            Exclusive React JS with styled components where you will learn why
-            we need this & how to use it
-          </P>
-          <ButtonContainer>
-            <a href="#">Watch now</a>
-            <a href="#">Github Repo</a>
-          </ButtonContainer>
-        </Content>
-        <Image src={imgSrc} alt="img" width="250px" />
-      </Card>
-    </Container>
+    <>
+      <GlobalStyle/>
+      <Container>
+        <Card>
+          <Content>
+            <Tag color="#4361ee">EXCLUSIVE</Tag>
+            <H1>React Styled Components</H1>
+            <P>
+              Exclusive React JS with styled components where you will learn why
+              we need this & how to use it
+            </P>
+            <ButtonContainer>
+              <a href="#">Watch now</a>
+              <a href="#">Github Repo</a>
+            </ButtonContainer>
+          </Content>
+          <Image src={imgSrc} alt="img" width="250px" />
+        </Card>
+      </Container>
+    </>
   );
 }
 
